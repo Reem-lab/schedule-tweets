d68d6e31da292bdc4962cfa1d14939c8ab827566
Reem <janinareem@gmail.com>
2022-05-17 13:45:25 +0200

    Update README file and the routes for the project

--- app/controllers/registeration_controller.rb
+++ app/controllers/registeration_controller.rb
@@ -15,6 +15,7 @@ class RegisterationController < ApplicationController
     end
   end
 
+  private
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
   end
